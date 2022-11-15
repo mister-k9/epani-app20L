22bc0176f6d542c7f188d6b844e941fd6f2c29ba
mister-k9 <nynkay9@gmail.com>
2022-11-15 15:55:53 +0530

    trying_new

--- gui/classes/ui.py
+++ gui/classes/ui.py
@@ -44,7 +44,7 @@ def create_footer_section(layout):
                      "color:white;")
     l1.setAlignment(Qt.AlignCenter)
     l1.setText(u"99K SOCIAL ENTERPRISE")
-    l1.setMinimumHeight(256)
+    l1.setMinimumHeight(360)
 
     l2.setFont(font)
     l2.setStyleSheet(u"background: black;\n"
